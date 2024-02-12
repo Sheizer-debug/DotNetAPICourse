0112da05a69f9f1e897761715bb6b51758a68ab9
Shreyan Saha <shreyan.saha@wiom.in>
2024-02-12 15:29:24 +0530

    Updated efcore file

--- personal_code/HelloWorld/Data/DataContextEF.cs
+++ personal_code/HelloWorld/Data/DataContextEF.cs
@@ -1,56 +1,34 @@
 
+
 using HelloWorld.Models;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.Configuration;
 
-namespace HelloWorld.Data{
+namespace HelloWorld.Data
+{
     public class DataContextEF : DbContext
     {
-        private IConfiguration _config;
-        public DataContextEF(IConfiguration config)
-        {
-            _config = config;
-        }
-        public DbSet<Computer>? Computer{get; set;}
+        public DbSet<Computer>? Computer { get; set; } 
         protected override void OnConfiguring(DbContextOptionsBuilder options)
         {
-            if(!options.IsConfigured)
+            if (!options.IsConfigured)
             {
-                options.UseSqlServer(_config.GetConnectionString("DefaultConnection"),
-                options => options.EnableRetryOnFailure());
+                // options.UseSqlServer("Server=localhost;Database=DotNetCourseDatabase;Trusted_connection=false;TrustServerCertificate=True;User Id=sa;Password=SQLConnect1;",
+                options.UseSqlServer("Server=localhost;Database=DotNetCourseDatabase;Trusted_Connection=true;TrustServerCertificate=true;",
+                    options => options.EnableRetryOnFailure());
             }
         }
+
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             modelBuilder.HasDefaultSchema("TutorialAppSchema");
-            modelBuilder.Entity<Computer>().HasKey(c => c.ComputerId);
-            //.HasNoKey();
-            //.HasKey(c => c.Motherboard);
-            //.ToTable("Computer","TutorialAppSchema");//tableName,schemaName
+
+            modelBuilder.Entity<Computer>()
+                // .HasNoKey()
+                .HasKey(c => c.ComputerId);
+                // .ToTable("Computer", "TutorialAppSchema");
+                // .ToTable("TableName", "SchemaName");
         }
-        // private string _connectionString = "Server=localhost;Database=DotNetCourseDatabase;TrustServerCertificate=true;Trusted_Connection=true;";
+        
 
-        // public IEnumerable<T> LoadData<T>(string sql)
-        // {
-        //     IDbConnection dbConnection = new SqlConnection(_connectionString);
-        //     return dbConnection.Query<T>(sql);
-        // }
-
-        // public T LoadDataSingle<T>(string sql)
-        // {
-        //     IDbConnection dbConnection = new SqlConnection(_connectionString);
-        //     return dbConnection.QuerySingle<T>(sql);
-        // }
-        // public bool ExecuteSql(string sql)
-        // {
-        //     IDbConnection dbConnection = new SqlConnection(_connectionString);
-        //     return dbConnection.Execute(sql) > 0;
-        // }
-
-        // public int ExecuteSqlWithRowCount(string sql)
-        // {
-        //     IDbConnection dbConnection = new SqlConnection(_connectionString);
-        //     return dbConnection.Execute(sql);
-        // }
     }
 }
